0da353367b26e86e5c3cbb5210c6391dd490f5d6
Ronit Kataria <ronitomaxe@gmail.com>
2025-07-17 22:06:06 +0530

    connection to mongodb atlas

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Backend series
+This is a series of backend 
+- [model link](https://app.eraser.io/workspace/yobrOCw2KyP9nuRVRqNK?origin=share)
---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB =async () => {
+    try {
+        const connectioninstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectioninstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection error", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
